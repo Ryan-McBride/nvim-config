37c16e3a2de524606ce9e7fb40c4a1cd027ebfd4
Ryan McBride <ryan.mcbride@kunaico.com>
2025-08-26 06:37:55 -0700

    pulling out nerdtree stuff that broke hotkeys, adding mason

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,23 @@
+return {
+  { "mason-org/mason.nvim", opts = {} },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = { "neovim/nvim-lspconfig" },
+    opts = {
+      ensure_installed = { "ts_ls", "lua_ls", "jsonls", "bashls" },
+      automatic_enable = true,
+    },
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+
+      local caps = require("cmp_nvim_lsp").default_capabilities()
+
+      vim.lsp.config("ts_ls", { capabilities = caps })
+      vim.lsp.config("lua_ls", { capabilities = caps })
+      vim.lsp.config("jsonls", { capabilities = caps })
+      vim.lsp.config("bashls", { capabilities = caps })
+
+      vim.lsp.config("tsserver", { capabilities = caps })
+    end,
+  },
+}
