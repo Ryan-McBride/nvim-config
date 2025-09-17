546cbb23c0e939d61f2d96c059c71a66bcb42daf
Ryan McBride <ryan.mcbride@kunaico.com>
2025-09-17 13:01:51 -0700

    fixing barbar to load on vimenter

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -4,12 +4,11 @@ return {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
   },
+  event = "VimEnter", -- Load barbar immediately on startup
   init = function() vim.g.barbar_auto_setup = false end,
   opts = {
-    -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
-    -- animation = true,
-    -- insert_at_start = true,
-    -- …etc.
+    auto_hide = false,
+    animation = true,
   },
   keys = {
     -- Restore original gt/gT behavior for visual tab order navigation
